0d1a96a3c7e490b525cb167c8a61552b71ce698b
xgarciaber <xgarciaber@educacion.navarra.es>
2026-01-19 20:21:07 +0100

    Descargadas librerias retrofit , y GSON

---
+++ app/src/main/java/com/example/navarres/model/network/RetrofitClient.kt
@@ -0,0 +1,17 @@
+// model/network/RetrofitClient.kt
+package com.example.navarres.model.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private const val BASE_URL = "https://api.tudominio.com/" // Tu URL base
+
+    val apiService: RestaurantApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create()) // Para convertir JSON a Objetos
+            .build()
+            .create(RestaurantApiService::class.java)
+    }
+}
